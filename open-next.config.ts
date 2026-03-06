030bbf3781d2ea2b0fd72daf98761512048a6d7f
Andres <andardg@gmail.com>
2026-03-06 01:27:24 +0100

    fix: simplify open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,14 +1,7 @@
 import type { OpenNextConfig } from "@opennextjs/cloudflare";
 
 const config: OpenNextConfig = {
-  // The default list of files to bundle in the worker
-  bundleFiles: true,
-  // Minify the worker bundle
-  minify: true,
-  // The build output directory
-  buildDir: ".open-next",
-  // Internal cache directory
-  cacheDir: ".open-next/cache",
+  default: {},
 };
 
 export default config;
